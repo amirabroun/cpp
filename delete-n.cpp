7f1f13d2dd44d4bc943d1e842905fe7f98269b39
amirabroun <abroon234@gmail.com>
2021-12-15 10:53:32 +0330

    9/24

--- delete-n.cpp
+++ delete-n.cpp
@@ -2,27 +2,26 @@
 using namespace std;
 int main()
 {
-    int n, adad, flag = 0;
+    int n, adad;
     cout << "enter n: ";
     cin >> n;
     cout << "enter adad: ";
     cin >> adad;
-    int a[n + 1];
+    int a[n];
     for (int i = 0; i < n; i++)
     {
         cout << "enter number(a): ";
         cin >> a[i];
     }
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i < n; i++)
     {
-        if (a[i] > adad)
+        if (a[i] == adad)
         {
-            a[i + 1] = a[i];
-            flag = i;
+            a[i] = a[n - 1];
+            break;
         }
     }
-    a[flag] = adad;
-    for (int i = 0; i < n + 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         cout << a[i] << endl;
     }
